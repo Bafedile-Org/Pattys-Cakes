4ff67dc709fa896c60713d070aa3705dd08ed6a3
gainsford-baloyi <gainsfordhlawulani@gmail.com>
2022-10-31 10:00:14 +0200

    initial commit

---
+++ PattyBakery/src/java/za/co/pattybakery/Payment.java
@@ -0,0 +1,9 @@
+
+package za.co.pattybakery;
+
+public interface Payment {
+    public void setAmount(String amount);
+    public String getAmount();
+    public void setEmail(String email);
+    public String getEmail();
+}
