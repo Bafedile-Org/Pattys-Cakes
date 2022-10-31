4cdd03dc8c537bd2d665a140ba0a6d5184a0c3f7
Bafedile <Ddimakatso30@gmail.com>
2022-10-31 10:01:32 +0200

    initial commit

---
+++ PattyBakery/src/java/za/co/pattybakery/exception/OrderException.java
@@ -0,0 +1,12 @@
+package za.co.pattybakery.exception;
+
+/**
+ *
+ * @author Dimakatso Sebatane
+ */
+public class OrderException extends Exception {
+
+    public OrderException(String msg) {
+        super(msg);
+    }
+}
