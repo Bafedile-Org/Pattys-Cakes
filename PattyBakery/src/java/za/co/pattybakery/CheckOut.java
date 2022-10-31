353b70c8ea5116b18545cf28e0cdde5773265601
Bafedile <Ddimakatso30@gmail.com>
2022-10-31 14:50:37 +0200

    updated

--- PattyBakery/src/java/za/co/pattybakery/CheckOut.java
+++ PattyBakery/src/java/za/co/pattybakery/CheckOut.java
@@ -1,13 +1,22 @@
-
 package za.co.pattybakery;
 
 import za.co.pattybakery.exception.CheckOutException;
 
 public interface CheckOut {
-    public void setShoppingCart(ShoppingCart shoppingCart)throws CheckOutException ;
+
+    String SHOPPING_CART_ERROR_MSG = "Shopping Cart Cannot Be Null!!";
+    String PAYMENT_ERROR_MSG = "Payment Can Not Be Null or Less Than The Amount Owning!!";
+    String ADDRESS_ERROR_MSG = "Address Can Not Be Null!!";
+
+    public void setShoppingCart(ShoppingCart shoppingCart) throws CheckOutException;
+
     public ShoppingCart getShoppingCart();
-    public void setPayment(Payment payment)throws CheckOutException ;
+
+    public void setPayment(Payment payment) throws CheckOutException;
+
     public Payment getPayment();
-    public void setAddress(String address)throws CheckOutException ;
+
+    public void setAddress(String address) throws CheckOutException;
+
     public String getAddress();
 }
--- PattyBakery/src/java/za/co/pattybakery/Payment.java
+++ PattyBakery/src/java/za/co/pattybakery/Payment.java
@@ -1,9 +1,17 @@
-
 package za.co.pattybakery;
 
+import za.co.pattybakery.exception.PaymentException;
+
 public interface Payment {
-    public void setAmount(String amount);
-    public String getAmount();
-    public void setEmail(String email);
+
+    String AMOUNT_ERROR_MSG = "Amount Can Not Be Less Than Zero!!!!";
+    String EMAIL_ERROR_MSG = "Delivery Email Can Not Be Null!!!";
+
+    public void setAmount(Double amount) throws PaymentException;
+
+    public Double getAmount();
+
+    public void setEmail(String email) throws PaymentException;
+
     public String getEmail();
 }
