41dbd3ea4d30361a77274bb7dfddccaea8879b2e
Bafedile <Ddimakatso30@gmail.com>
2022-11-01 12:55:31 +0200

    Revert "Merge branch 'Bridget-dev' into master-dev"
    This reverts commit 389267fa30affbd7fc654fa84c359c557c0f8af0, reversing
    changes made to 4713c6d4af7eb60cf36dc9599975912fec20b835.

--- PattyBakery/src/java/za/co/pattybakery/dao/impl/CategoryDAOImpl.java
+++ PattyBakery/src/java/za/co/pattybakery/dao/impl/CategoryDAOImpl.java
@@ -1,7 +1,10 @@
 package za.co.pattybakery.dao.impl;
 
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import za.co.pattybakery.dao.CategoryDAO;
 
@@ -13,30 +16,76 @@ public class CategoryDAOImpl implements CategoryDAO {
 
     private PreparedStatement preparedStatement;
     private ResultSet resultSet;
-
-    public CategoryDAOImpl(PreparedStatement preparedStatement, ResultSet resultSet) {
-        this.preparedStatement = preparedStatement;
-        this.resultSet = resultSet;
+    private Connection con = null;
+   
+    public CategoryDAOImpl(){
     }
-
     @Override
     public void addCategory(String category) {
-
+        try{
+            if(con != null){
+                preparedStatement = con.prepareStatement("INSERT INTO category (category) VALUES(?)");
+                preparedStatement.setString(1, category);
+                preparedStatement.executeUpdate();
+            }
+        }
+        catch(SQLException sql){
+            System.out.println("Error occured..!!" + sql.getMessage());
+        }
+        finally{
+            close(preparedStatement, resultSet);
+        }
     }
 
     @Override
     public void updateCategory(Integer categoryId, String category) {
-
+        try{
+            if(con != null){
+                preparedStatement = con.prepareStatement("UPDATE category SET category =? WHERE categoryId = ?");
+                preparedStatement.setString(1, category);
+                preparedStatement.setInt(2, categoryId);
+                preparedStatement.executeQuery();
+            }
+        }catch(SQLException sql){
+            System.out.println("Error: " + sql.getMessage());       
+        }
+        finally{
+            close(preparedStatement, resultSet);
+        }
     }
 
     @Override
     public void removeCategory(Integer categoryId) {
-
+        try{
+            if(con != null){
+                preparedStatement = con.prepareStatement("DELETE FROM category WHERE categoryId = ?");
+                preparedStatement.setInt(1, categoryId);
+                preparedStatement.executeQuery();
+            }
+        }
+        catch(SQLException sql){
+            System.out.println("Error: " + sql.getMessage());
+        }
     }
-
     @Override
     public List<String> getAllCategory() {
-        return getAllCategory();
+        List<String> categories = new ArrayList<>();
+        
+        try{
+        if(con != null){
+            preparedStatement = con.prepareStatement("SELECT * FROM category");
+            resultSet = preparedStatement.executeQuery();
+            while(resultSet.next()){
+            categories.add(resultSet.getString("category"));
+             }
+        }
     }
-
+    catch(SQLException sql){
+        System.out.println("Error: " + sql.getMessage());
+    }
+        finally{
+        close(preparedStatement, resultSet);
+    }
+        return categories;
+    }
 }
