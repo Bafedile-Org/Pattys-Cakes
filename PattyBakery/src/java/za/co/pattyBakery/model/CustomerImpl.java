d325f2ea7d2c37b92fe2a640f6dd7f72ea9c17e7
Bafedile <Ddimakatso30@gmail.com>
2022-11-01 12:56:25 +0200

    Revert "Revert "Merge branch 'Bridget-dev' into master-dev""
    This reverts commit 41dbd3ea4d30361a77274bb7dfddccaea8879b2e.

---
+++ PattyBakery/src/java/za/co/pattyBakery/model/CustomerImpl.java
@@ -0,0 +1,17 @@
+package za.co.pattybakery.model;
+
+/**
+ *
+ * @author Bridget Bapela
+ */
+public class CustomerImpl extends PersonImpl {
+
+    public CustomerImpl(String name, String surname, String idNumber, String address, String tel, String email) {
+        super(name, surname, idNumber, address, tel, email);
+    }
+
+    public CustomerImpl(String name, String surname, String idNumber, String address, String tel, String email, Integer personId) {
+        super(personId, name, surname, idNumber, address, tel, email);
+    }
+
+}
