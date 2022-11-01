0ac5ca2c203ae73fdc78af24fce2e2ed94c32c4f
Bafedile <Ddimakatso30@gmail.com>
2022-11-01 12:56:44 +0200

    Revert "Revert "Revert "Merge branch 'Bridget-dev' into master-dev"""
    This reverts commit d325f2ea7d2c37b92fe2a640f6dd7f72ea9c17e7.

--- PattyBakery/src/java/za/co/pattybakery/dao/impl/IngredientsDAOImpl.java
+++ PattyBakery/src/java/za/co/pattybakery/dao/impl/IngredientsDAOImpl.java
@@ -1,7 +1,10 @@
 package za.co.pattybakery.dao.impl;
 
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import za.co.pattybakery.dao.IngredientsDAO;
 
@@ -9,34 +12,85 @@ import za.co.pattybakery.dao.IngredientsDAO;
  *
  * @author Hlawulani
  */
-public class IngredientsDAOImpl implements IngredientsDAO {
+public class IngredientsDAOImpl implements IngredientsDAO{
+    private PreparedStatement ps;
+    private ResultSet rs;
+    private Connection con = null;
 
-    private PreparedStatement preparedStatement;
-    private ResultSet resultSet;
-
-    public IngredientsDAOImpl(PreparedStatement preparedStatement, ResultSet resultSet) {
-        this.preparedStatement = preparedStatement;
-        this.resultSet = resultSet;
+    public IngredientsDAOImpl() {
+       
     }
 
     @Override
     public void addIngrideint(String ingredient) {
-
+        try{
+            if(con != null){
+                ps = con.prepareStatement("INSERT INTO ingredient (ingredient) VALUE(?)");
+                ps.setString(1, ingredient);
+                ps.executeQuery();
+            }
+        }
+        catch(SQLException sql){
+            System.out.println("Error: " + sql.getMessage());
+        }
+        finally{
+            close(ps,rs);
+        }
     }
 
     @Override
     public void updateIngredient(Integer ingredientId, String ingredient) {
-
+        try{
+            if(con != null){
+                ps = con.prepareStatement("UPDATE ingredient SET ingredient = ? WHERE ingredientId = ?");
+                ps.setString(1, ingredient);
+                ps.setInt(2, ingredientId);
+                ps.executeQuery();
+            }
+        }
+        catch(SQLException sql){
+            System.out.println("Error: " + sql.getMessage());
+        }
+        finally{
+            close(ps,rs);
+        }
     }
 
     @Override
     public void removeIngredient(Integer ingredientId) {
-
+        try{
+            if(con != null){
+                ps = con.prepareStatement("DELETE FROM ingredient WHERE ingredientId = ?");
+                ps.setInt(1, ingredientId);
+                ps.executeQuery();
+            }
+        }catch(SQLException sql){
+            System.out.println("Error: " + sql.getMessage());
+        }
+        finally{
+            close(ps,rs);
+        }
     }
 
     @Override
     public List<String> getAllIngredient() {
-        return null;
+        List<String> ingridiets = new ArrayList<>();
+        try{
+            if(con != null){
+                ps = con.prepareStatement("SELECT * FROM ingredient");
+                rs = ps.executeQuery();
+                while(rs.next()){
+                    ingridiets.add(rs.getString("ingredient"));
+                }
+            }
+        }
+        catch(SQLException sql){
+            System.out.println("Error: " + sql.getMessage());
+        }
+        finally{
+            close(ps,rs);
+        }
+        return ingridiets;
     }
 
 }
